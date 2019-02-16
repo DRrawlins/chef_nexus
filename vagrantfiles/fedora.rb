f6334fdc67610d5f7372b24ec1883068ca257fb4
drawlins-gl <drawlins@greenlancer.com>
2019-02-16 15:39:52 -0500

    quality of life changes

--- vagrantfiles/fedora.rb
+++ vagrantfiles/fedora.rb
@@ -6,7 +6,7 @@ required_plugins = %w{
   vagrant-vbguest
 }
 required_plugins.each do |plugin|
-  exec "vagrant plugin install #{plugin};vagrant #{ARGV.join(' ')}" unless Vagrant.has_plugin? plugin || ARGV[0] == 'plugin'
+  exec "vagrant plugin install #{plugin};vagrant #{ARGV.join(' ')}" unless (Vagrant.has_plugin? plugin) || ARGV[0] == 'plugin'
 end
 
 Vagrant.configure(2) do |config|

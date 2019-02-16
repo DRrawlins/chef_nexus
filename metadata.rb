2bf760d14f9c6746b67ce7831eedf524a7fc6ae8
drawlins-gl <drawlins@greenlancer.com>
2019-02-16 15:49:44 -0500

    metadata bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'danrawlins07@gmail.com'
 license          'MIT'
 description 'Installs/Configures chef_nexus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 
 %w{fedora}.each do |os|
   supports os

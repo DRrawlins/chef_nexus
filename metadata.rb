7e363d1c5a5d36a464a2df4665687ead138f089a
Daniel Rawlins <danrawlins07@gmail.com>
2019-02-16 15:14:06 -0500

    initial commit of generically generated and needed files

---
+++ metadata.rb
@@ -0,0 +1,16 @@
+name 'chef_nexus'
+maintainer 'Daniel Rawlins'
+maintainer_email 'danrawlins07@gmail.com'
+license          'MIT'
+description 'Installs/Configures chef_nexus'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
+%w{fedora}.each do |os|
+  supports os
+end
+
+source_url 'https://github.com/DRrawlins/chef_nexus'
+issues_url 'https://github.com/DRrawlins/chef_nexus/issues'
+
+chef_version '>= 14.10.9'
